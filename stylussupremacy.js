ad9ffaedec64ceb2763207865d7eca20f18b1519
Aktanov Redal <aktanoff@gmail.com>
2020-02-03 18:01:28 +0500

    fix

--- stylussupremacy.js
+++ stylussupremacy.js
@@ -3,14 +3,12 @@ const getStdin = require("get-stdin");
 const stylusSupremacy = require("stylus-supremacy");
 
 (async () => {
-	const data = await getStdin();
-    process.stdout.write(process.argv[2]);
-    return
+  const data = await getStdin();
   const formattingOptions = JSON.parse(process.argv[2]);
 
-	try {
-		process.stdout.write(stylusSupremacy.format(data, formattingOptions));
-	} catch (err) {
-		throw err;
-	}
+  try {
+    process.stdout.write(stylusSupremacy.format(data, formattingOptions));
+  } catch (err) {
+    throw err;
+  }
 })();
